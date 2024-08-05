85a0868dbd5aeb768ce163047e181a1dc5f306e0
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-08-05 11:21:17 -0300

    feat: adicionado prefixo da entidade e alterado tipagem de matrícula do aluno

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('professor/', views.get_professores, name='get_all_professores'),
-    path('professor/<int:id_professor>/', views.get_by_id_professor, name='get_by_id_professor'),
-    path('professor/cadastrar/', views.criar_professor, name='criar_professor'),
-    path('aluno/', views.get_all_alunos, name='get_all_alunos'),
-    path('aluno/<str:id_professor>/', views.get_by_matricula_aluno, name='get_by_matricula_aluno'),
+    path('professor', views.get_professores, name='get_all_professores'),
+    path('professor/<int:id_professor>', views.get_by_id_professor, name='get_by_id_professor'),
+    path('professor/cadastrar', views.criar_professor, name='criar_professor'),
+    path('aluno', views.get_all_alunos, name='get_all_alunos'),
+    path('aluno/<str:matricula>', views.get_by_matricula_aluno, name='get_by_matricula_aluno'),
     path('aluno/cadastrar/', views.criar_aluno, name='criar_aluno'),
 ]
