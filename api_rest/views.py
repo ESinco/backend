c22215b855a42b8b1f97fc6308dd259a338742c0
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-08-05 12:50:06 -0300

    feat: adicionado views para cadastro de Projetos e exibição deles a partir do ID ou do Professor ou todos os Projetos

--- api_rest/views.py
+++ api_rest/views.py
@@ -44,3 +44,53 @@ def criar_professor(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['POST'])
+def criar_projeto(request):
+    if request.method == 'POST':
+        serializer = ProjetoSemIdSerializer(data=request.data)
+
+        if serializer.is_valid():
+            projeto = serializer.save()
+            response_serializer = ProjetoSerializer(projeto)
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def get_projetos(request):
+    if request.method == 'GET':
+        projetos = Projeto.objects.all()
+        serializer = ProjetoSerializer(projetos, many=True)
+        return Response(serializer.data)
+
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def get_by_id_projeto(request, id_projeto):
+    try:
+        projeto = Projeto.objects.get(pk=id_projeto)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProjetoSerializer(projeto)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def get_all_projetos_by_professor(request):
+    if request.method == 'GET':
+        responsavel = request.GET['responsavel']
+        if responsavel is not None:
+            try:
+                projetos = Projeto.objects.filter(responsavel__id_professor=responsavel)
+                serializer = ProjetoSerializer(projetos, many=True)
+                return Response(serializer.data)
+            except Projeto.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({"detail": "O parâmetro 'responsavel' é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
