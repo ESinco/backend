6624775b5309b290cbeed7170adb4354811353ec
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-08-05 10:23:25 -0300

    feat: criado models para Aluno, Professor e Projeto

---
+++ api_rest/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Professor(models.Model):
+    id_professor = models.AutoField(primary_key=True)
+    nome = models.TextField()
+    email = models.TextField(unique=True)
+    senha = models.TextField()
+
+    def __str__(self):
+        return (f'id_Professor: {self.id_professor}\n'
+                f'nome: {self.nome}\n'
+                f'email: {self.email}\n'
+                f'senha: {self.senha}')
+
+class Aluno(models.Model):
+    matricula = models.CharField(max_length=9, primary_key=True)
+    nome = models.TextField()
+    email = models.TextField(unique=True)
+    senha = models.TextField()
+
+    def __str__(self):
+        return (f'matricula: {self.matricula}\n'
+                f'nome: {self.nome}\n'
+                f'email: {self.email}\n'
+                f'senha: {self.senha}')
+
+class Projeto(models.Model):
+    id_projeto = models.AutoField(primary_key=True)
+    nome = models.TextField()
+    descricao = models.TextField()
+
+    def __str__(self):
+        return (f'id_projeto: {self.id_projeto}\n'
+                f'nome: {self.nome}\n'
+                f'descrição: {self.descricao}')
