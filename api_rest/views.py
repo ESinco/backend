5354c45bc23c36e006a5271b350b6dfb7c9d384f
Andre Felipe <andre.neves@ccc.ufcg.edu.br>
2024-08-14 10:47:59 -0300

    fix: consertado conflitos de merge

--- api_rest/views.py
+++ api_rest/views.py
@@ -17,8 +17,9 @@ def criar_professor(request):
         serializer = ProfessorSemIdSerializer(data=novo_professor)
 
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            professor = serializer.save()
+            response_serializer = ProfessorSerializer(professor)
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -52,8 +53,8 @@ def criar_aluno(request):
         serializer = AlunoSerializer(data=novo_aluno)
 
         if serializer.is_valid():
-            professor = serializer.save()
-            response_serializer = ProfessorSerializer(professor)
+            aluno = serializer.save()
+            response_serializer = AlunoSerializer(aluno)
             return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
