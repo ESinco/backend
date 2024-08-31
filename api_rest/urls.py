22fca9c2219a074976613da79476f6ac8fd407f7
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-08-31 03:01:16 -0300

    feat: adicionado URL para login de professor

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('professor/', views.get_professores, name='get_all_professores'),
     path('professor/cadastrar/', views.criar_professor, name='criar_professor'),
     path('professor/<int:id_professor>/', views.get_by_id_professor, name='get_by_id_professor'),
+    path('professor/login/', views.login_professor, name='login_professor'),
     path('aluno/', views.get_all_alunos, name='get_all_alunos'),
     path('aluno/cadastrar/', views.criar_aluno, name='criar_aluno'),
     path('aluno/<str:matricula>/', views.get_by_matricula_aluno, name='get_by_matricula_aluno'),
