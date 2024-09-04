13c82fddeb87b608e0ae8b8b7a6f7e4ac2e5ce8c
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-03 21:05:55 -0300

    feat: criado novo serializer para POST de Projeto, alterado formatação de 'data_de_criacao' do projeto para retorno de requisições, alterado ProfessorPostSerializer para criar um 'User' do Django com as informações do cadastro

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from .models import Professor, Aluno, Projeto
 
 
@@ -7,23 +8,42 @@ class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = '__all__'
+        
+class ProfessorPostSerializer(serializers.ModelSerializer):
+    senha = serializers.CharField(write_only=True)
 
-class ProfessorSemIdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = ['nome', 'email', 'senha']
 
+    def create(self, validated_data):
+        senha = validated_data.pop('senha')
+        usuario = User.objects.create_user(
+            username=validated_data['email'],
+            email=validated_data['email'],
+            password=senha
+        )
+        professor = Professor.objects.create(user=usuario, **validated_data)
+        return professor
+    
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = '__all__'
 
 class ProjetoSerializer(serializers.ModelSerializer):
+    data_de_criacao = serializers.DateTimeField(format="%d/%m/%Y")
+    
     class Meta:
         model = Projeto
         fields = '__all__'
 
-class ProjetoSemIdSerializer(serializers.ModelSerializer):
+class ProjetoSemIdSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Projeto
         fields = ['nome', 'descricao', 'laboratorio', 'data_de_criacao', 'vagas', 'responsavel']
+        
+class ProjetoPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projeto
+        fields = ['nome', 'descricao', 'laboratorio', 'vagas']
