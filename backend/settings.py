cd0cbe201843787ebc10300ebe47f676b9826310
Andre Felipe <andre.neves@ccc.ufcg.edu.br>
2024-09-06 13:41:36 -0300

    style: adicionado tag para o compilador ignorar importação

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from django.conf import settings
+from django.conf import settings # type: ignore
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
