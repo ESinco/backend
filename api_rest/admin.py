048e66c4fba526a8c63e911bee3d12a3f2d0d016
Andre Felipe <andre.neves@ccc.ufcg.edu.br>
2024-09-06 15:11:58 -0300

    refactor: alterado para registrar entidades no site do ADMIN a partir de cada API correspondente

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Professor, Aluno, Projeto, HistoricoAcademico
-
-admin.site.register(Professor)
-admin.site.register(Aluno)
-admin.site.register(Projeto)
-admin.site.register(HistoricoAcademico)
