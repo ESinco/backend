47b4086343e4095032945c2ac6f36eadf6670ada
Andre Felipe <andre.neves@ccc.ufcg.edu.br>
2024-09-06 15:12:37 -0300

    feat: criado aplicação para aluno e adicionado permissão de ADMIN para registrar entidades

---
+++ api_aluno/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Aluno, HistoricoAcademico
+
+admin.site.register(Aluno)
+admin.site.register(HistoricoAcademico)
---
+++ api_aluno/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiAlunoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_aluno'
