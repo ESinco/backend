f0cc514b4421aa3e0b633b3ce5955d01de9ef97f
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-06 21:12:23 -0300

    refactor: realocado serializers de Projeto

---
+++ api_projeto/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Projeto
+
+
+class ProjetoSerializer(serializers.ModelSerializer):
+    data_de_criacao = serializers.DateTimeField(format="%d/%m/%Y")
+    
+    class Meta:
+        model = Projeto
+        fields = '__all__'
+
+class ProjetoSemIdSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Projeto
+        fields = ['nome', 'descricao', 'laboratorio', 'data_de_criacao', 'vagas', 'responsavel']
+        
+class ProjetoPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projeto
+        fields = ['nome', 'descricao', 'laboratorio', 'vagas']
