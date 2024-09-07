6e077b6c2528e6ca27edb49a57c89c4b8a025364
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-06 21:12:17 -0300

    refactor: realocado model de Projeto

--- api_projeto/models.py
+++ api_projeto/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from api_professor.models import Professor  
+from api_aluno.models import Aluno
 
-# Create your models here.
+
+class Projeto(models.Model):
+    id_projeto = models.AutoField(primary_key=True)
+    nome = models.TextField()
+    descricao = models.TextField(null=True)
+    laboratorio = models.TextField(null=True)
+    vagas = models.IntegerField(null=True)
+    data_de_criacao = models.DateTimeField(default=timezone.now)
+    responsavel = models.ForeignKey(Professor, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f'nome: {self.nome}\n'
+                f'descrição: {self.descricao}\n'
+                f'laboratório: {self.laboratorio}\n'
+                f'data de criação: {self.data_de_criacao.strftime("%d/%m/%Y")}\n'
+                f'vagas: {self.vagas}\n'
+                f'responsavel: {self.responsavel}')
+        
+class Associacao(models.Model):
+    id_associacao = models.AutoField(primary_key=True)
+    projeto = models.ForeignKey(Projeto, null= False, on_delete=models.CASCADE)
+    aluno = models.ForeignKey(Aluno, null=False, on_delete=models.CASCADE)
+    status = models.TextField(null=True)
