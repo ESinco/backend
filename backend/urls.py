4b94d3cf159a7a999784dae3474964bd782d4828
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-06 21:14:20 -0300

    feat: adicionado URLs das novas aplicações e de renovar um Token de acesso

--- backend/urls.py
+++ backend/urls.py
@@ -1,23 +1,13 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_rest.urls'), name='api_rest_urls'),
+    path('token/atualizar/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('professor/', include('api_professor.urls'), name='api_professor_urls'),
+    path('aluno/', include('api_aluno.urls'), name='api_aluno_urls'),
+    path('projeto/', include('api_projeto.urls'), name='api_projeto_urls')
 ]
