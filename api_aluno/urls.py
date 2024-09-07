7dc1d5a0ccdf07729ca595fec1c111104351064c
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-06 21:08:34 -0300

    refactor: realocado URLs de Aluno

---
+++ api_aluno/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.get_all_alunos, name='get_all_alunos'),
+    path('cadastrar/', views.criar_aluno, name='criar_aluno'),
+    path('login/', views.login_aluno, name='login_aluno'),
+    path('<str:matricula>/', views.get_by_matricula_aluno, name='get_by_matricula_aluno'),
+    path('historico/importar/', views.upload_historico, name='upload_historico'),
+    path('historico/<str:matricula>/', views.visualizar_historico, name='visualizar_historico'),
+]
