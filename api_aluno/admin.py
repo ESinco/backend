555dbb583fa7b8183a4428ff6ff0510cd40e5df0
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-06 21:07:43 -0300

    style: adicionado linha entre importações e código

--- api_aluno/admin.py
+++ api_aluno/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Aluno, HistoricoAcademico
 
+
 admin.site.register(Aluno)
 admin.site.register(HistoricoAcademico)
--- api_projeto/admin.py
+++ api_projeto/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Projeto
 
+
 admin.site.register(Projeto)
