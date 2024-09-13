2b268be60401452a7e999e4c8a1f4386e39271cd
Carmelita Braga <carmelitabraga8@gmail.com>
2024-09-13 10:56:36 -0300

    chore: create Dockerfile and docker compose

--- backend/settings.py
+++ backend/settings.py
@@ -107,10 +107,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ProjetIn-db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
