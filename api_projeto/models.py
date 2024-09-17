204c4d5dcfe001c0f1da2297cf7e3dbe9a4f7e49
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-16 22:03:36 -0300

    feat: retirado Filtro e adicionado Lista de Filtragens como model

--- api_projeto/models.py
+++ api_projeto/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 from api_professor.models import Professor  
-from api_aluno.models import Aluno
-
+from api_aluno.models import Aluno, Disciplina
+from api_rest.models import Habilidade, Experiencia, Interesse
 
 class Projeto(models.Model):
     id_projeto = models.AutoField(primary_key=True)
@@ -27,14 +28,37 @@ class Associacao(models.Model):
     aluno = models.ForeignKey(Aluno, null=False, on_delete=models.CASCADE)
     status = models.TextField(null=True)
     
-class Filtro(models.Model):
+class Lista_Filtragem(models.Model):
     id_filtro = models.AutoField(primary_key=True)
+    id_projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
     id_professor = models.ForeignKey(Professor, null=False, on_delete=models.CASCADE)
-    id_projeto = models.ForeignKey(Projeto, null=False, on_delete=models.CASCADE)
-    criterios = models.JSONField()
-    alunos = models.ManyToManyField(Aluno, related_name="lista_filtradas")
+    titulo = models.TextField(null=False)
+    filtro_habilidades = models.ManyToManyField(Habilidade, related_name="filtros", blank=True)
+    filtro_experiencias = models.ManyToManyField(Experiencia, related_name="filtros", blank=True)
+    filtro_interesses = models.ManyToManyField(Interesse, related_name="filtros", blank=True)
+    filtro_disciplinas = models.JSONField(blank=True, null=False)
+    filtro_cra = models.FloatField(null=True)
     
-    def __str__(self):
-        return (f'Filtro {self.id_filtro}\n'
-               f'Professor {self.id_professor}\n'
-               f'Projeto {self.id_projeto}\n')
+    def clean(self):
+        super().clean()
+        self.validate_filtro_disciplinas()
+
+    def validate_filtro_disciplinas(self):
+        if not isinstance(self.filtro_disciplinas, list):
+            raise ValidationError("O campo 'filtro_disciplinas' deve ser uma lista de objetos.")
+        
+        for item in self.filtro_disciplinas:
+            if not isinstance(item, dict):
+                raise ValidationError("Cada item de 'filtro_disciplinas' deve ser um JSON.")
+            if "disciplina" not in item or "nota" not in item:
+                raise ValidationError("Cada item deve ter as chaves 'disciplina' e 'nota'.")
+            if not isinstance(item["disciplina"], str):
+                raise ValidationError("'disciplina' deve ser uma string.")
+            if not isinstance(item["nota"], (int, float)):
+                raise ValidationError("'nota' deve ser um número.")
+            if item["nota"] < 0 or item["nota"] > 10:
+                raise ValidationError("'nota' deve estar entre 0 e 10.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
