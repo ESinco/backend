87394842aa0eb665d310f190fab7d6e99b827078
luanabringel <luanabringelleite@gmail.com>
2024-09-16 23:02:39 -0300

    feat: adicionando url para editar projeto

--- api_projeto/urls.py
+++ api_projeto/urls.py
@@ -10,5 +10,6 @@ path('cadastrar/csv', views.criar_projeto_csv, name='criar_projeto_csv'),
 path('', views.get_projetos, name='get_projetos'),
 path('<int:id_projeto>/', views.get_by_id_projeto, name='get_by_id_projeto'),
 path('professor/', views.get_all_projetos_by_professor, name='get_all_projetos_by_professor'),
-path('aluno/', views.get_all_projetos_by_aluno, name='get_all_projetos_by_aluno')
+path('aluno/', views.get_all_projetos_by_aluno, name='get_all_projetos_by_aluno'),
+path('<int:id_projeto>/editar/', views.editar_projeto, name='editar_projeto')
 ]
