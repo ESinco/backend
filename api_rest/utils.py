37dc99f0103fad5629faab46d3ecee26a17aa232
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-17 01:24:31 -0300

    fix: consertado lógica para salvar disciplinas equivalentes

--- api_rest/utils.py
+++ api_rest/utils.py
@@ -27,8 +27,9 @@ def salvar_disciplinas(disciplinas):
         codigo_da_disciplina = disciplina['codigo_da_disciplina']
         existe_disciplina = Disciplina.objects.filter(pk=codigo_da_disciplina).exists()
         if not existe_disciplina:
+            disciplinas_equivalentes = [eq['codigo_da_disciplina'] for eq in disciplina['disciplinas_equivalentes']]
             disciplina = Disciplina.objects.create(
                 codigo=codigo_da_disciplina,
                 nome=disciplina['nome_da_disciplina'],
-                disciplinas_equivalentes=disciplina['disciplinas_equivalentes']
+                disciplinas_equivalentes=disciplinas_equivalentes
             )
