bbf296b2816726232abc36fa035e36eaa5576ba2
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-16 22:04:03 -0300

    feat: adicionado URL para ciração de uma lista de filtros

--- api_projeto/urls.py
+++ api_projeto/urls.py
@@ -11,9 +11,5 @@ path('', views.get_projetos, name='get_projetos'),
 path('<int:id_projeto>/', views.get_by_id_projeto, name='get_by_id_projeto'),
 path('professor/', views.get_all_projetos_by_professor, name='get_all_projetos_by_professor'),
 path('aluno/', views.get_all_projetos_by_aluno, name='get_all_projetos_by_aluno'),
-path('filtrar/disciplina/', views.filtragem_disciplina, name='filtragem_disciplina'),
-path('filtrar/cra/', views.filtragem_cra, name='filtragem_cra'),
-path('filtrar/habilidade/', views.filtragem_habilidade, name='filtragem_habilidade'),
-path('filtrar/experiencia/', views.filtragem_experiencia, name='filtragem_experiencia'),
-path('filtrar/interesse/', views.filtragem_interesse, name='filtragem_interesse'),
+path('cadastrar-filtro/', views.salvar_filtragem, name='salvar_filtragem')
 ]
