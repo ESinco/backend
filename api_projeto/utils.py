fe624cbb33aa3a42da746cde45b6d90e4e1b8be7
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-19 21:46:15 -0300

    feat: criado um util para envio de e-mail

---
+++ api_projeto/utils.py
@@ -0,0 +1,39 @@
+from django.core.mail import send_mail
+
+def enviar_email(aluno, projeto, aceito):
+    if aceito:
+        assunto = "Resposta de Inscrição no Projeto"
+        mensagem = (
+            f"Olá {aluno.nome},\n\n"
+            f"Agradecemos o seu interesse no projeto {projeto.nome}. "
+            "Infelizmente, você não passou para a próxima etapa.\n\n"
+            "Esperamos vê-lo em outro processo de seleção no futuro!\n\n"
+            "Atenciosamente,\nEquipe do ProjetIn."
+        )
+        destinatario = [aluno.email]
+        
+        send_mail(
+            assunto,
+            mensagem,
+            'projetinufcg@gmail.com',
+            destinatario,
+            fail_silently=False,
+        )
+        
+    else:
+        assunto = "Resposta de Inscrição no Projeto"
+        mensagem = (
+            f"Olá {aluno.nome},\n\n"
+            f"Agradecemos o seu interesse no projeto {projeto.nome}. "
+            "Felizmente, você passou para a próxima etapa.\n\n"
+            "Atenciosamente,\nEquipe do ProjetIn."
+        )
+        destinatario = [aluno.email]
+        
+        send_mail(
+            assunto,
+            mensagem,
+            'projetinufcg@gmail.com',
+            destinatario,
+            fail_silently=False,
+        )        
